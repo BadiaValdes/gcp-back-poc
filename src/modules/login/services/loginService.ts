f3b52535b4684b8f3a537d1e6c833e739a4e4804
Julio Santana <juliosantana3009@gmail.com>
2024-02-15 20:34:53 -0300

    act

--- src/modules/login/services/loginService.ts
+++ src/modules/login/services/loginService.ts
@@ -26,10 +26,10 @@ class LoginService implements ILoginService {
   res: any
 
   auth = getAuth(app)
-  user: User;
-  userCredential: UserCredential;
+  user: User
+  userCredential: UserCredential
 
-  async login (body: any): Promise<any> {
+  async login(body: any): Promise<any> {
     return new Promise<successResponse>((resolve, reject) => {
       const login = {
         email: body.email,
@@ -37,14 +37,35 @@ class LoginService implements ILoginService {
       }
       signInWithEmailAndPassword(this.auth, login.email, login.password)
         .then((userCredential) => {
-          this.userCredential = userCredential;
+          this.userCredential = userCredential
           this.user = userCredential.user
           resolve({ status: 200, body: userCredential, message: 'Login exitosamente' })
         })
-        .catch((error) => {
+        .catch((error: any) => {
+          console.log(error)
           if (error.code == 'auth/multi-factor-auth-required') {
             // The user is a multi-factor user. Second factor challenge is required.
-            const resolver = getMultiFactorResolver(this.auth, error);
+            const resolver = getMultiFactorResolver(this.auth, error)
+            console.log(resolver)
+            /*const recaptchaVerifier = new RecaptchaVerifier(
+              this.auth,
+              'recaptcha-container-id',
+              undefined
+            )*/
+            resolver.hints.forEach((hint: any) => {
+              if (hint.factorId == 'phone') {
+                //const provider = new PhoneAuthProvider(this.auth)
+                //const phoneInfoOptions = {
+                //  multiFactorHint: hint,
+                //  session: resolver.session,
+                //}
+                /*provider
+                  .verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier)
+                  .then((verificationId) => {
+                    console.log(verificationId)
+                  })*/
+              }
+            })
             reject({ status: 400, message: resolver })
             // ...
           } else if (error.code == 'auth/wrong-password') {
@@ -55,7 +76,7 @@ class LoginService implements ILoginService {
     })
   }
 
-  asyncUpdatePassword (password: string) {
+  asyncUpdatePassword(password: string) {
     console.log(password)
     return new Promise<successResponse>((resolve, reject) => {
       updatePassword(this.user, password)
@@ -69,19 +90,20 @@ class LoginService implements ILoginService {
     })
   }
 
-  async login2 (body: any): Promise<any> {
+  async login2(body: any): Promise<any> {
     return new Promise<successResponse>((resolve, reject) => {
-    
-      console.log("HERE")
+      console.log('HERE')
 
-      const provider = new GoogleAuthProvider();
+      const provider = new GoogleAuthProvider()
 
-      signInWithRedirect(this.auth, provider).then(result => {
-        console.log(result)
-        resolve({ status: 200, body: "HOLA", message: 'Login exitosamente' })
-      }).catch((error) => {
-        reject({ status: 400, message: error.message })
-      })
+      signInWithRedirect(this.auth, provider)
+        .then((result) => {
+          console.log(result)
+          resolve({ status: 200, body: 'HOLA', message: 'Login exitosamente' })
+        })
+        .catch((error) => {
+          reject({ status: 400, message: error.message })
+        })
 
       // signInWithEmailAndPassword(this.auth, login.email, login.password)
       //   .then((userCredential) => {
@@ -94,12 +116,12 @@ class LoginService implements ILoginService {
     })
   }
 
-  loginWithPopup () {
-    const provider = new GoogleAuthProvider();
+  loginWithPopup() {
+    const provider = new GoogleAuthProvider()
     signInWithPopup(this.auth, provider).then((result) => {
       console.log(result)
-    });
- 
+    })
+
     // The signed-in user info.
     //const user = result.user;
     // This gives you a Facebook Access Token.
@@ -107,42 +129,46 @@ class LoginService implements ILoginService {
     //const token = credential.accessToken;
   }
 
-  getUserInformation () {
-    return getAdditionalUserInfo(this.userCredential);
+  getUserInformation() {
+    return getAdditionalUserInfo(this.userCredential)
   }
 
-  multiFactor () {
-    const recaptchaVerifier = new RecaptchaVerifier(this.auth, '');
-    multiFactor(this.user).getSession()
+  multiFactor() {
+    const recaptchaVerifier = new RecaptchaVerifier(this.auth, '')
+    multiFactor(this.user)
+      .getSession()
       .then(function (multiFactorSession) {
         // Specify the phone number and pass the MFA session.
         const phoneInfoOptions = {
-          phoneNumber: "+59898335948",
-          session: multiFactorSession
-        };
+          phoneNumber: '+59898335948',
+          session: multiFactorSession,
+        }
 
-        const phoneAuthProvider = new PhoneAuthProvider(this.auth);
+        const phoneAuthProvider = new PhoneAuthProvider(this.auth)
 
         // Send SMS verification code.
-        return phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier);
-      }).then(function (verificationId) {
+        return phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier)
+      })
+      .then(function (verificationId) {
         // Ask user for the verification code. Then:
-        const cred = PhoneAuthProvider.credential(verificationId, "1234");
-        const multiFactorAssertion = PhoneMultiFactorGenerator.assertion(cred);
+        const cred = PhoneAuthProvider.credential(verificationId, '1234')
+        const multiFactorAssertion = PhoneMultiFactorGenerator.assertion(cred)
 
         // Complete enrollment.
-        return multiFactor(this.user).enroll(multiFactorAssertion, 'My second factor');
-      });
+        return multiFactor(this.user).enroll(multiFactorAssertion, 'My second factor')
+      })
   }
 
-  changePassword (email: string) {
+  changePassword(email: string) {
     return new Promise<successResponse>((resolve, reject) => {
-      const auth = getAuth(app);
-      sendPasswordResetEmail(auth, email).then(() => {
-        resolve({ status: 200, body: "HOLA", message: 'Cambio de password' })
-      }).catch((error) => {
-        reject({ status: 400, message: error.message })
-      });
+      const auth = getAuth(app)
+      sendPasswordResetEmail(auth, email)
+        .then(() => {
+          resolve({ status: 200, body: 'HOLA', message: 'Cambio de password' })
+        })
+        .catch((error) => {
+          reject({ status: 400, message: error.message })
+        })
     })
   }
 }
