9027c30a9bb9ee4b5c9afdaccbec53869715d155
emilio <jebv.informatico@gmail.com>
2024-02-26 14:06:13 -0500

    refactor: login router to controller

---
+++ src/helpers/db-dummy.ts
@@ -0,0 +1,10 @@
+export const DB_DUMMY = [
+    {
+        email: "juliosantana3009@gmail.com",
+        dobleFac: true,
+    },
+    {
+        email: "jebv9606@gmail.com",
+        dobleFac: false,
+    },
+]
