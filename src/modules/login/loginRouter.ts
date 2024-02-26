8d929c1ca3b4ae44cc076111749d228e76a2644f
emilio <jebv.informatico@gmail.com>
2024-02-26 13:40:00 -0500

    refactor: login router to controller

--- src/modules/login/loginRouter.ts
+++ src/modules/login/loginRouter.ts
@@ -9,7 +9,6 @@ class LoginRouter implements IRouter {
   loginController = loginController;
   // eslint-disable-line
   get routes() {
-   
     router.post("/", async (req: Request, res: Response) => {
       return this.loginController.login(req, res);
     });
@@ -18,15 +17,18 @@ class LoginRouter implements IRouter {
       return await loginController.changePassword(req, res);
     });
 
-    router.post("/userInformation", async (req: Request, res: Response) => {
+    router.post("/user-information", async (req: Request, res: Response) => {
       return await this.loginController.getUserInformation(req, res);
     });
 
-    router.post("/changePassword", async (req: Request, res: Response) => {
+    router.post("/change-password", async (req: Request, res: Response) => {
       return await this.loginController.sendMailPasswordReset(req, res);
     });
 
-   
+    router.post("/verify-code", async (req: Request, res: Response) => {
+      return this.loginController.verifyTwoStepCode(req, res);
+    });
+
     return router;
   }
 }
